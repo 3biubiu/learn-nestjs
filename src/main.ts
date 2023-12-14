c5b4e5d9681044abeab0170919fad19501c1f274
zhangheng <zhangheng@zhaoshang.net>
2023-12-14 18:52:35 +0800

    init

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useStaticAssets('public',{prefix: '/static'})
+  await app.listen(7878);
+}
+bootstrap();
