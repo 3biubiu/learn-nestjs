79c0eadd21fb9fc231764324470c7468da38ac52
3biubiu <2227358943@qq.com>
2023-12-14 23:00:24 +0800

    4

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,18 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+}
+
+@Controller('api/person')
+export class PersonController {
+  // query 要放在urlparam前面 nest 从上往下匹配 上面的优先匹配
+  @Get('find')
+  query(@Query('name') name:string, @Query('age') age:number) {
+    return `received: name=${name},age=${age}`
+  }
+  @Get(':id')
+  urlParam(@Param('id') id:string) {
+    return `id = ${id}`
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { AppController,PersonController } from './app.controller';
 import { AppService } from './app.service';
 import { PersonModule } from './person/person.module';
 
 @Module({
   imports: [PersonModule],
-  controllers: [AppController],
+  controllers: [AppController,PersonController ],
   providers: [AppService],
 })
 export class AppModule {}
